11afa48163a058ed5eb7dc6635669a3608bc4d59
“why” <why@163.com>
2019-05-11 21:14:14 +0800

    创建Cat类

---
+++ weibo6/weibo6/Cat.h
@@ -0,0 +1,17 @@
+//
+//  Cat.h
+//  weibo6
+//
+//  Created by wind on 2019/5/11.
+//  Copyright © 2019 JLK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cat : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ weibo6/weibo6/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  weibo6
+//
+//  Created by wind on 2019/5/11.
+//  Copyright © 2019 JLK. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
